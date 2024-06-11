e7da6c565c42bb76066338fcd8372671bb7aaa06
Debiddo <dzheng@terpmail.umd.edu>
2024-06-10 22:45:44 -0400

    upgrade commit

--- Front-end-Buzzpage/src/components/SinglePost/SinglePost.jsx
+++ Front-end-Buzzpage/src/components/SinglePost/SinglePost.jsx
@@ -74,44 +74,47 @@ const SinglePost = () => {
     return (
         <div className={styles.container}>
             {post &&
-                <div className="card" style={{ width: "40rem" }}>
-
+                <div className="card" style={{ width: "30rem" }}>
                     <img src={post.photo} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <div className='like-buttons'>
-                            {post.like} Likes
-                            <button className="btn btn-primary m-1" onClick={() => handleLikeClick("like")}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-suit-heart-fill" viewBox="0 0 16 16">
-                                    <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
-                                </svg>                            </button>
-                            <button className="btn btn-danger m-1" onClick={() => handleLikeClick("dislike")}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heartbreak-fill" viewBox="0 0 16 16">
-                                    <path d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586M7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77" />
-                                </svg>
-                            </button>
+                        <div className="d-flex gap-1 mb-2">
+                            <div className='like-buttons'>
+                                {post.like} Likes
+                                <button className="btn btn-primary mx-1" onClick={() => handleLikeClick("like")}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-suit-heart-fill" viewBox="0 0 16 16">
+                                        <path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
+                                    </svg>
+                                </button>
+                                <button className="btn btn-danger" onClick={() => handleLikeClick("dislike")}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heartbreak-fill" viewBox="0 0 16 16">
+                                        <path d="M8.931.586 7 3l1.5 4-2 3L8 15C22.534 5.396 13.757-2.21 8.931.586M7.358.77 5.5 3 7 7l-1.5 3 1.815 4.537C-6.533 4.96 2.685-2.467 7.358.77" />
+                                    </svg>
+                                </button>
+                            </div>
+                            {currentUser && currentUser._id === post.author._id && (
+                                <>
+                                    <button className="btn btn-success">
+                                        <Link
+                                            className={`text-light ${styles.edit}`}
+                                            to={`/users/${currentUser._id}/posts/${postId}/edit`}
+                                        >
+                                            Edit Post
+                                        </Link>
+                                    </button>
+                                    <button
+                                        className="btn btn-danger"
+                                        onClick={() => props.handleDeletePost(post._id)}> Delete Post
+                                    </button>
+                                </>
+                            )}
                         </div>
-                        {currentUser && currentUser._id === post.author._id && (
-                            <>
-                                <button className="btn">
-                                    <Link
-                                        to={`/users/${currentUser._id}/posts/${postId}/edit`}
-                                    >
-                                        Edit
-                                    </Link>
-                                </button>
-                                <button
-                                    className="btn"
-                                    onClick={() => props.handleDeletePost(post._id)}> Delete
-                                </button>
-                            </>
-                        )}
                         <h5 className="card-title">{post.title} by {post.author.username}</h5>
                         <p className="card-text">{post.text}</p>
 
                         {/* Collapse button and back to hive */}
                         <div className="d-inline-flex gap-1">
                             <button
-                                className="btn btn-secondary mb-2"
+                                className={`btn btn-link mb-2 ${styles.link}`}
                                 type="button"
                                 data-bs-toggle="collapse"
                                 data-bs-target="#collapseExample"
@@ -122,57 +125,60 @@ const SinglePost = () => {
 
                             </button>
 
-                            <button className="btn btn-secondary mb-2" onClick={handleClick}>Back to the Hive
-                            </button>
-
                         </div>
 
                         <div className="collapse" id="collapseExample">
                             <div className="card card-body">
                                 <div className="comment-section">
-                                    <p className='text-center'>Comment Sections</p>
+                                    <h3 className='text-center'>Comments</h3>
                                     <div className="comments">
                                         {post.comments &&
                                             post.comments.map((comment, index) => (
-                                                <li key={index}>
+                                                <div key={index}>
                                                     {editCommentId === comment._id ? (
-                                                        <>
-                                                            <input
-                                                                type="text"
-                                                                value={editedCommentText.text}
-                                                                onChange={(e) =>
-                                                                    setEditedCommentText({ text: e.target.value })
-                                                                }
-                                                            />
-                                                            <button onClick={() => handleSaveEdit(comment._id)}>
-                                                                Save
-                                                            </button>
-                                                        </>
+                                                        <div className='border p-3'>
+                                                            <p>{comment.authorName}:
+                                                                <input
+                                                                    type="text"
+                                                                    value={editedCommentText.text}
+                                                                    onChange={(e) =>
+                                                                        setEditedCommentText({ text: e.target.value })
+                                                                    }
+                                                                />
+                                                                <button className='mx-1' onClick={() => handleSaveEdit(comment._id)}>
+                                                                    Save
+                                                                </button>
+                                                            </p>
+                                                        </div>
                                                     ) : (
-                                                        <>
-                                                            {comment.text}
+                                                        <div className='d-flex flex-column border p-3'>
+                                                            <p>
+                                                                {comment.authorName}: {comment.text}
+                                                            </p>
                                                             {currentUser && currentUser._id === post.author._id && (
-                                                                <>
+                                                                <div className='d-flex gap-1 align-items-baseline'>
                                                                     <button
                                                                         onClick={() =>
                                                                             handleToggleEdit(comment._id, comment.text)
                                                                         }
+                                                                        className={`btn-link ${styles.btn}`}
                                                                     >
                                                                         Edit
                                                                     </button>
                                                                     <button
+                                                                        className={`btn-link ${styles.btn}`}
                                                                         onClick={() =>
                                                                             handleDeleteComment(comment._id)
                                                                         }
                                                                     >
                                                                         Delete
                                                                     </button>
-                                                                </>
+                                                                </div>
                                                             )}
-                                                        </>
+                                                        </div>
                                                     )
                                                     }
-                                                </li>
+                                                </div>
                                             ))}
                                     </div>
                                 </div>
